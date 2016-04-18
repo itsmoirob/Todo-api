645d377125bd0f3676821e85913b9b23524f305d
Robbie Sherman <rob.w.sherman@gmail.com>
2016-04-18 19:32:16 +0100

    Added ?completed filter

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ app.get('/', function(req, res) {
 
 // GET TODOs
 app.get('/todos', function(req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(todos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(todos, {completed: false})
+    } 
+    
+    res.json(filteredTodos);
 });
 
 // get TODOs/:id
