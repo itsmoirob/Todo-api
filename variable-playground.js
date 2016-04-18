e2d24cb8692a34af955dfdbd33868e5baceda15c
Robbie Sherman <rob.w.sherman@gmail.com>
2016-04-18 19:32:59 +0100

    Add varaible playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+//     name: 'Robbie',
+//     age:31
+// };
+
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name: 'Robbie',
+//     //     age: 34
+//     // };
+//     obj.age = 34;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [21,23];
+
+function addGrades(grades) {
+    grades.push(55);
+    debugger;
+};
+
+addGrades(grades);
+console.log(grades);
