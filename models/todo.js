6e2d2b9519ef137797c8b8ce114777c9964e4c41
Robbie Sherman <rob.w.sherman@gmail.com>
2016-08-21 17:52:59 +0100

    Update PUT /todos/:id to use sequlize

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,24 @@
-module.exports = function(sequelize, Datatypes) {
-    return sequelize.define('todo', {
-        description: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 250]
-            }
-        },
-        completed: {
-            type: Datatypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        }
-    });
+module.exports = function (sequelize, Datatypes) {
+	return sequelize.define('todo', {
+		description: {
+			type: Datatypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 250]
+			}
+		},
+		completed: {
+			type: Datatypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	}, {
+			validate: {
+				descriptionIsString: function () {
+					if (!_.isString(this.description)) {
+						throw new Error('Description must be a string');
+					}
+				}
+			}
+		});
 };
