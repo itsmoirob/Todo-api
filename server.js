e42fcd627c4e0360362888a1507db10d92868aee
Robbie Sherman <rob.w.sherman@gmail.com>
2016-09-09 14:31:52 +0100

    add in force true to wipe heroku db

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post('/user/login', function (req, res) {
 
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
 	app.listen(PORT, function () {
 		console.log('Express listening on PORT: ' + PORT);
 	})
